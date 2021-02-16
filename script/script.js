a03a81b4de18a465cae89d2cf4c041a3f81be583
Vitaliy <vitaliysomonyuk@gmail.com>
2021-02-16 18:40:44 +0200

    update file script.js

--- script/script.js
+++ script/script.js
@@ -11,13 +11,29 @@ const personalMovieDB = {
     privat: false
 };
 
-let nameFilm1 = prompt("Один з останніх переглянутих фільмів?", "");
-let ratingOfFilms1 = prompt("На скільки ви його оціните?", "10");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один з останніх переглянутих фільмів?", ""),
+          b = prompt("На скільки ви його оціните?", "10");
 
-let nameFilm2 = prompt("Один з останніх переглянутих фільмів?", "");
-let ratingOfFilms2 = prompt("На скільки ви його оціните?", "10");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 
-personalMovieDB.movies[nameFilm1] = ratingOfFilms1;
-personalMovieDB.movies[nameFilm2] = ratingOfFilms2;
+
+if (personalMovieDB.count < 10) {
+    alert('Переглянуто мало фільмів)');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Ви класичний глядач!');
+} else if (personalMovieDB.count > 30) {
+    alert('Ви кіноман! =)');
+} else {
+    alert('Невідома помилка');
+}
+  
+}
 
 console.log(personalMovieDB);
