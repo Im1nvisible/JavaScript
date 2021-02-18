c5d93ef644a9b13d6c9217bf1fc5ed4e42b95dbc
Vitaliy <vitaliysomonyuk@gmail.com>
2021-02-18 23:11:03 +0200

    add file scriptМethod.js

---
+++ script/scriptMethod.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const str = "Test";
+
+console.log(str[2]);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+
+
+const logg = "Hey there";
+console.log(logg.slice(4, 9));
+console.log(logg.substring(4, 9));
+console.log(logg.substr(4, 5));
+
+
+const num = 12.3;
+console.log(Math.round(num));
+
+
+const tes = "12.2px";
+console.log(parseInt(tes));
+console.log(parseFloat(tes));
