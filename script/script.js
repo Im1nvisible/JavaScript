d8ea7e6c0e0e219fa562a1017089fbd617484f92
Vitaliy <vitaliysomonyuk@gmail.com>
2021-02-19 21:51:27 +0200

    modified file script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
 'use strict';
 
 
-let numberOfFilms =  +prompt("Скільки ви фільмів подивились?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms =  +prompt("Скільки ви фільмів подивились?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms =  +prompt("Скільки ви фільмів подивились?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,29 +21,57 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один з останніх переглянутих фільмів?", ""),
-          b = prompt("На скільки ви його оціните?", "10");
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один з останніх переглянутих фільмів?", ""),
+              b = prompt("На скільки ви його оціните?", "10");
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
-
-
-if (personalMovieDB.count < 10) {
-    alert('Переглянуто мало фільмів)');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Ви класичний глядач!');
-} else if (personalMovieDB.count > 30) {
-    alert('Ви кіноман! =)');
-} else {
-    alert('Невідома помилка');
-}
-  
 }
 
-console.log(personalMovieDB);
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+        alert('Переглянуто мало фільмів)');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Ви класичний глядач!');
+    } else if (personalMovieDB.count > 30) {
+        alert('Ви кіноман! =)');
+    } else {
+        alert('Невідома помилка');
+    }
+}
+
+detectPersonalLevel();
+
+
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш улюблений жанр під номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
+
